8fc51cef5447fe13b349400bc767d137e661798e
Flavore669 <dirkosh2@gmail.com>
2025-08-19 18:01:05 -0500

    minor changes

--- ldtk-entity-configure/level-import-script/level_import_script.gd
+++ ldtk-entity-configure/level-import-script/level_import_script.gd
@@ -11,7 +11,6 @@ func post_import(level: LDTKLevel) -> LDTKLevel:
 	# Instead of calling import while it's not intialized. Create a toggle to import at runtime and set it here
 	if entity_layer:
 		entity_importer.entity_dict = EXAMPLE
-		entity_importer.entity_layer = entity_layer
 		entity_importer.import(entity_layer)
 	else:
 		print("Error: No Entity Layer Found in Scene Tree")
